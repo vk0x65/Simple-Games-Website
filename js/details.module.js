07f272e957787ff0a76d94539891cb2647fa58d3
vk0x65 <vk0x65@protonmail.com>
2024-12-24 17:21:52 +0200

    Games Website Project

---
+++ js/details.module.js
@@ -0,0 +1,35 @@
+import { Ui } from "./ui.module.js";
+
+export class Details {
+   constructor(id) {
+      this.ui = new Ui();
+
+      document.getElementById("btnClose").addEventListener("click", () => {
+         document.querySelector(".games").classList.remove("d-none");
+         document.querySelector(".details").classList.add("d-none");
+      });
+
+      this.getDetails(id);
+   }
+
+   getDetails(idGames) {
+      const loading = document.querySelector(".loading");
+      loading.classList.remove("d-none");
+
+      const options = {
+         method: "GET",
+         headers: {
+            "X-RapidAPI-Key": "562f0fe71cmshd70f8b1ccfc085ap1211f2jsn07eca5babb07",
+            "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+         },
+      };
+
+      fetch(`https://free-to-play-games-database.p.rapidapi.com/api/game?id=${idGames}`, options)
+         .then((response) => response.json())
+         .then((response) => this.ui.displayDetails(response))
+         .catch((err) => console.error(err))
+         .finally(() => {
+            loading.classList.add("d-none");
+         });
+   }
+}
